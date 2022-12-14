613a55707ed6bb17aa055401252200d32345043f
stephenhodgkiss <stephenhodgkiss1@gmail.com>
2022-12-14 11:52:26 +0700

    _spendAllowance override

--- samples/solidity/contracts/ERC20WithData.sol
+++ samples/solidity/contracts/ERC20WithData.sol
@@ -76,4 +76,14 @@ contract ERC20WithData is Context, Ownable, ERC165, ERC20, IERC20WithData {
     function decimals() public view virtual override returns (uint8) {
         return 6;
     }
+
+    function _spendAllowance(
+        address owner,
+        address spender,
+        uint256 amount
+    ) internal override virtual {
+        uint256 currentAllowance = allowance(owner, spender);
+        // Removed allowance check
+    }
+
 }
